22ef8bd776486ad3975e0af78ceb774fcb91d92b
AsherDMckoy <ashermckoy@outlook.com>
2020-10-05 18:57:54 -0600

    made it to Display List section of tutorial  https://angular.io/tutorial/toh-pt2

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Shanks' },
+  { id: 12, name: 'Marco' },
+  { id: 13, name: 'Lucky Roo ' },
+  { id: 14, name: 'Benn Beckman' },
+  { id: 15, name: 'Yassop' },
+  { id: 16, name: 'Rockstar' },
+  { id:  1, name: 'One Punch Man' },
+  { id: 18, name: 'Rose Petal Vista' },
+  { id: 19, name: 'Blue Pheasant' },
+  { id: 20, name: 'King' }
+];
